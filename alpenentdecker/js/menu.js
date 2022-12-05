2bf1b1be6962cdb11e0f14feabeb1088bd83c3ee
superernie77 <superernie77@gmail.com>
2022-12-05 10:45:45 +0100

    reworked menü

--- alpenentdecker/js/menu.js
+++ alpenentdecker/js/menu.js
@@ -5,14 +5,14 @@ var autocollapse = function (menu,maxHeight) {
     // menü ist 2 zeilsen groß => element ist umgebrochen worden => in das menü setzen
     if (navHeight >= maxHeight) {
         
-        $(menu + ' .dropdown').removeClass('d-none');
+        $(menu + ' .collapser').removeClass('d-none');
         $(".navbar-nav").removeClass('w-auto').addClass("w-100");
         
         while (navHeight > maxHeight) {
             //  add child to dropdown
             var children = nav.children(menu + ' li:not(:last-child)');
             var count = children.length;
-            $(children[count - 1]).prependTo(menu + ' .dropdown-menu');
+            $(children[count - 1]).prependTo(menu + ' .collapseitems');
             navHeight = nav.innerHeight();
         }
         $(".navbar-nav").addClass("w-auto").removeClass('w-100');
@@ -21,15 +21,15 @@ var autocollapse = function (menu,maxHeight) {
     // menü ist nur eine zeile groß
     else {
         
-        var collapsed = $(menu + ' .dropdown-menu').children(menu + ' li');
+        var collapsed = $(menu + ' .collapseitems').children(menu + ' li');
       
         if (collapsed.length===0) {
-          $(menu + ' .dropdown').addClass('d-none');
+          $(menu + ' .collapser').addClass('d-none');
         }
       
         while (navHeight <= maxHeight && (nav.children(menu + ' li').length > 0) && collapsed.length > 0) {
             //  remove child from dropdown
-            collapsed = $(menu + ' .dropdown-menu').children('li');
+            collapsed = $(menu + ' .collapseitems').children('li');
             $(collapsed[0]).insertBefore(nav.children(menu + ' li:last-child'));
             navHeight = nav.innerHeight();
         }
